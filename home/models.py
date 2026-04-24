b4bb000adc5a8f72b74c329a6d91819b6e1f8747
Nasreen Shah <nasreenshah704@gmail.com>
2026-04-24 22:58:59 +0530

    Create Product model

--- home/models.py
+++ home/models.py
@@ -5,3 +5,12 @@ class Contact(models.Model):
     name = models.CharField(max_length=122)
     email = models.CharField(max_length=122)
     description = models.TextField()
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    image = models.ImageField(upload_to='products/')
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
